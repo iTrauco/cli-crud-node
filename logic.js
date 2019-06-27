b3d75011b901bd833278cb772b5ea64eb8eb8d1b
Christopher Trauco <Chris@Trau.co>
2019-06-27 03:00:09 -0400

    useNewUrlParser debug deprecation errorhttp://bit.ly/2YfxdKQ

--- logic.js
+++ logic.js
@@ -1,51 +1,56 @@
-const mongoose = require('mongoose')  // an object document-wrapper for node
-const assert = require('assert'); // Assert module is bundled w/ node
-mongoose.Promise = global.Promise; // Permits use of native promises w/o throwing shady errors
+const mongoose = require('mongoose'); // An Object-Document Mapper for Node.js
+const assert = require('assert'); // N.B: Assert module comes bundled with Node.js.
+mongoose.Promise = global.Promise; // Allows us to use Native promises without throwing error.
 
-// connect to a single MongoDB instance, the connection string could be that of a remote server
-// we assign the connection instance to a constant to be used later in closing the connection 
+// Connect to a single MongoDB instance. The connection string could be that of a remote server
+// We assign the connection instance to a constant to be used later in closing the connection
+mongoose.connect('mongodb://localhost:27017/contact-manager');
+const db = mongoose.connection;
 
-// define the 'contact' schema
-const constactChema = mongoose.Schema({
-    firstname: {type: String, set: toLower},
-    lastname: {type: String, set: toLower},
-    phone: {type: String, set: toLower},
-    email: {type: String, set: toLower}
+
+// Converts value to lowercase
+function toLower(v) {
+  return v.toLowerCase();
+}
+
+// Define a contact Schema
+const contactSchema = mongoose.Schema({
+  firstname: { type: String, set: toLower },
+  lastname: { type: String, set: toLower },
+  phone: { type: String, set: toLower },
+  email: { type: String, set: toLower }
 });
 
-// define model as an interface with the db
+// Define model as an interface with the database
 const Contact = mongoose.model('Contact', contactSchema);
 
 /**
- * @function  [addContact] // controller function
+ * @function  [addContact]
  * @returns {String} Status
  */
-
 const addContact = (contact) => {
-    Contact.create(contact, (err) => {
-        assert.equal(null, err);
-        console.info('New contact added');
-        db.disconnect();
-    });
+  Contact.create(contact, (err) => {
+    assert.equal(null, err);
+    console.info('New contact added');
+    db.close();
+  });
 };
 
-
-/** 
- * @function  [getContact] // controller function
+/**
+ * @function  [getContact]
  * @returns {Json} contacts
  */
+const getContact = (name) => {
+  // Define search criteria. The search here is case-insensitive and inexact.
+  const search = new RegExp(name, 'i');
+  Contact.find({$or: [{firstname: search }, {lastname: search }]})
+  .exec((err, contact) => {
+    assert.equal(null, err);
+    console.info(contact);
+    console.info(`${contact.length} matches`);
+    db.disconnect();
+  });
+};
 
- const getContact = (name) => {
-     // define search criteria. the search here is case-insensitive and inexact 
-     const search = new RegExp(name, 'i');
-    Contact.find({$or: [{firstname: search}, {lastname: search}]})
-    .exec((err, contact) => {
-        assert.equal(null, err);
-        console.info(contact);
-        console.info(`${contact.length} matches`);
-        db.disconnect();
-    });
- };
-
- // export all methods
- module.exports = { addContact, getContact };
+// Export all methods
+module.exports = {     addContact, getContact };
